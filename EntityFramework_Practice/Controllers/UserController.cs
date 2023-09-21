3fdf4cc620bf466edbd61d3b905cd56b0b630a9b
hash-amit <hash.amitkumar@gmail.com>
2023-09-21 20:05:04 +0530

    Insert Update Delete using DbSet instead of using stored procedures' funtion

--- EntityFramework_Practice/Controllers/UserController.cs
+++ EntityFramework_Practice/Controllers/UserController.cs
@@ -20,7 +20,12 @@ namespace EntityFramework_Practice.Controllers
         {
             if (user.User_ID == 0)
             {
-                dbEntities.InsertUser(user.Name, user.Email, user.Age);
+                // Inserting data into table using DbSet
+                dbEntities.tblUsers.Add(user);
+
+                // Inserting data into table using stored procedure's method
+                // dbEntities.InsertUser(user.Name, user.Email, user.Age); 
+                
                 dbEntities.SaveChanges();
             }
             else
@@ -32,19 +37,35 @@ namespace EntityFramework_Practice.Controllers
 
         public JsonResult GetAllUser()
         {
-            var data = dbEntities.GetAllUser().ToList();
+            // Retrieving all data from the table using DbSet
+            var data = dbEntities.tblUsers.ToList();
+
+            // Retrieve all data from the table using a custom method
+            // var data = dbEntities.GetAllUser().ToList();
+            
             return Json(data,JsonRequestBehavior.AllowGet);
         }
 
         public JsonResult GetOneUser(tblUser user)
         {
-            var data = dbEntities.GetOneUser(user.User_ID).ToList();
+            // Retereving one record using DbSet and through lambda function telling which record it has to retereve
+            var data = dbEntities.tblUsers.Where(M => M.User_ID == user.User_ID).ToList();
+
+            // Using custom method to retreve only one record
+            // var data = dbEntities.GetOneUser(user.User_ID).ToList();
+            
             return Json(data, JsonRequestBehavior.AllowGet);
         }
 
         public void DeleteUser(tblUser user)
         {
-            dbEntities.DeleteUser(user.User_ID);
+            /* Retereving one record which we have to delete because Remove() takes full record instead of only ID */
+            var data = dbEntities.tblUsers.Find(user.User_ID);
+            dbEntities.tblUsers.Remove(data);
+
+            // Using custom method to delete one record from table
+            // dbEntities.DeleteUser(user.User_ID);
+            
             dbEntities.SaveChanges();
         }
     }
