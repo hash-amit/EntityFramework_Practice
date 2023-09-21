1d713b10f8b54f77a7ae13b663ee1f81476baae1
hash-amit <hash.amitkumar@gmail.com>
2023-09-21 15:39:26 +0530

    Edit User

--- EntityFramework_Practice/Controllers/UserController.cs
+++ EntityFramework_Practice/Controllers/UserController.cs
@@ -16,16 +16,30 @@ namespace EntityFramework_Practice.Controllers
             return View("UserForm");
         }
 
-        public void InsertUser(tblUser user)
+        public void InsertUpdateUser(tblUser user)
         {
-            dbEntities.InsertUser(user.Name, user.Email, user.Age);
-            dbEntities.SaveChanges();
+            if (user.User_ID == 0)
+            {
+                dbEntities.InsertUser(user.Name, user.Email, user.Age);
+                dbEntities.SaveChanges();
+            }
+            else
+            {
+                dbEntities.UpdateUser(user.User_ID, user.Name, user.Email, user.Age);
+                dbEntities.SaveChanges();
+            }
         }
 
         public JsonResult GetAllUser()
         {
             var data = dbEntities.GetAllUser().ToList();
             return Json(data,JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetOneUser(tblUser user)
+        {
+            var data = dbEntities.GetOneUser(user.User_ID).ToList();
+            return Json(data, JsonRequestBehavior.AllowGet);
         }
     }
 }
